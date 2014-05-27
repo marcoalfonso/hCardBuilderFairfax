e99570ac28a985b46e3662ee9fb3a9ec69d9f10f
Marco Lavielle <contact@marcolavielle.com>
2014-05-28 05:06:43 +1000

    initial commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/**
+ * AngularJS module to process the form.
+ */
+angular.module('hCard', [])
+  .controller('data', function ($scope, $http) {
+
+    // Form submit handler.
+    $scope.submit = function(form) {    
+    };
+  });
